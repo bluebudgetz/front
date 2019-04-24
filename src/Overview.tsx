bef3d7802de6af7287678f4fc04465c9c67cce18
Eric Kfir <eric@kfirs.com>
2019-04-24 18:17:32 +0300

    Update text in home pageˆ

--- src/Overview.tsx
+++ src/Overview.tsx
@@ -5,31 +5,16 @@ class Overview extends React.Component {
     public render() {
         return (
             <div>
-                <Typography variant="h3" gutterBottom={true}>Overview</Typography>
+                <Typography variant="h3" gutterBottom={true}>Home</Typography>
 
-                <Typography variant="body1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                    et dolore magna aliqua. Fames ac turpis egestas integer eget aliquet nibh praesent. Fringilla
-                    phasellus faucibus scelerisque eleifend. Pulvinar etiam non quam lacus. Quisque non tellus orci ac.
-                    Velit euismod in pellentesque massa placerat. Nunc id cursus metus aliquam eleifend mi in nulla
-                    posuere. Mattis vulputate enim nulla aliquet. Auctor neque vitae tempus quam pellentesque nec nam
-                    aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Aliquam faucibus purus in massa
-                    tempor nec feugiat nisl pretium. Venenatis urna cursus eget nunc scelerisque viverra mauris. Quis
-                    hendrerit dolor magna eget. Ut pharetra sit amet aliquam id diam. Augue ut lectus arcu bibendum at.
-                    Enim facilisis gravida neque convallis. Aliquet porttitor lacus luctus accumsan tortor. Cursus in
-                    hac habitasse platea dictumst.
+                <Typography variant="headline" gutterBottom={true}>
+                    Welcome to Bluebudgetz, your one stop shop to home accounting, done right!
                 </Typography>
 
                 <Typography variant="body1">
-                    Aliquam etiam erat velit scelerisque in dictum non consectetur. Massa id
-                    neque aliquam vestibulum
-                    morbi blandit cursus risus at. Pellentesque diam volutpat commodo sed egestas egestas. Erat
-                    imperdiet sed euismod nisi porta lorem mollis aliquam ut. Viverra mauris in aliquam sem fringilla.
-                    Nisi scelerisque eu ultrices vitae. Non consectetur a erat nam at lectus. Purus sit amet volutpat
-                    consequat mauris nunc. Imperdiet dui accumsan sit amet. Molestie ac feugiat sed lectus vestibulum
-                    mattis. Viverra tellus in hac habitasse platea. Tellus orci ac auctor augue mauris augue neque
-                    gravida in. Imperdiet sed euismod nisi porta lorem mollis aliquam ut. Amet dictum sit amet justo
-                    donec enim. Risus ultricies tristique nulla aliquet enim tortor.
+                    Bluebudgetz aims to take away the tedious parts of your accounting work, automatically parsing your
+                    bank & credit card transactions, giving you an overview that's always accurate - and to help you
+                    get <b>actionable insights</b>.
                 </Typography>
             </div>
         );
