dce2b0b5b2a4eb47654eb7c81f09510c64f49fd0
Eric Kfir <eric@kfirs.com>
2019-05-11 19:55:31 +0300

    Add "handshake" icon from Font Awesomeˆ

--- src/app/icons.module.ts
+++ src/app/icons.module.ts
@@ -2,7 +2,15 @@ import {NgModule} from '@angular/core';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {faBalanceScale, faBars, faCog, faInfoCircle, faListAlt, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {
+    faBalanceScale,
+    faBars,
+    faCog,
+    faHandshake,
+    faInfoCircle,
+    faListAlt,
+    faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
 import {faCopyright} from "@fortawesome/free-regular-svg-icons";
 
 @NgModule({
@@ -12,6 +20,16 @@ import {faCopyright} from "@fortawesome/free-regular-svg-icons";
 })
 export class IconsModule {
     constructor() {
-        library.add(faGithub, faCopyright, faLinkedin, faBars, faBalanceScale, faListAlt, faCog, faInfoCircle, faUserCircle);
+        library.add(
+            faGithub,
+            faCopyright,
+            faLinkedin,
+            faBars,
+            faBalanceScale,
+            faListAlt,
+            faCog,
+            faInfoCircle,
+            faUserCircle,
+            faHandshake);
     }
 }
