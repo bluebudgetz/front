ad76ef4b1f0e83022ca0ddc04f2389fc8cfc4bec
Eric Kfir <eric@kfirs.com>
2019-05-11 19:55:18 +0300

    Add localesˆ

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
+import {registerLocaleData} from "@angular/common";
+
+import english from '@angular/common/locales/en';
+import hebrew from '@angular/common/locales/he';
 
 if (environment.production) {
     enableProdMode();
 }
+
+registerLocaleData(english, 'en-US');
+registerLocaleData(hebrew, 'he-IL');
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
