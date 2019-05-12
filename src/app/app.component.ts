a2eb9dc16ee773838e30126b934e3d6207009032
Eric Kfir <eric@kfirs.com>
2019-05-12 18:00:17 +0300

    Switch to PrimeFlex from FlexLayout, and move menu to a sidebar drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    sideBarOpen = false;
+    menu = [
+        { path: "/accounts", icon: "balance-scale", text: "Balance" },
+        { path: "/transactions", icon: "list-alt", text: "Transactions" },
+        { path: "/settings", icon: "cog", text: "Settings" },
+        { path: "/about", icon: "info-circle", text: "About" },
+    ];
 }
