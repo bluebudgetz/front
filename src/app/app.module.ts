37f4e436d58a9a4efef9951253b2edb9de9aad80
Arik Kfir <arik@kfirs.com>
2019-12-06 16:58:38 +0200

    Add Font Awesome icons library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,25 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
+import {FaIconLibrary, FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
+    declarations: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         MaterialModule,
+        FontAwesomeModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
 })
 export class AppModule {
+    constructor(library: FaIconLibrary) {
+        library.addIconPacks(fas, far, fab);
+    }
 }
