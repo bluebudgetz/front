ef2df4a4a2e27506552752f2c63934011c576cd1
Arik Kfir <arik@kfirs.com>
2019-12-06 15:47:30 +0200

    Incorporate Material UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'front';
+    sideBarOpen = false;
+    menu = [
+        {path: '/accounts', icon: 'account_balance', text: 'Balance'},
+        {path: '/transactions', icon: 'view_list', text: 'Transactions'},
+        {path: '/settings', icon: 'settings', text: 'Settings'},
+        {path: '/about', icon: 'info', text: 'About'},
+    ];
 }
